c46cca11e08c4829b06ed7248371da743d8dd0c0
han880805@gmail.com <kix99aug@gmail.com>
2019-01-07 18:46:28 +0800

    add google oauth2

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
+var Session = require('express-session')
+var google = require('googleapis')
+var oauth2 = google.oauth2_v2.Oauth2
+var secret = require('./secret.json')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+132132
 var app = express();
+
+const redirection = 'https://contact.csie.nuk.edu.tw/login'
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +27,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(Session({
+  secret: '803b49765bbd574d2ee22f70b565dd661daafc5e',
+  resave: true,
+  saveUninitialized: true
+}));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
