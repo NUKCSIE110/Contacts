8b546d9e1601038525ebb569378a558d394a207e
han880805@gmail.com <kix99aug@gmail.com>
2019-01-12 22:45:13 +0800

    改超多東西就對了 = =

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,6 @@ const redirection = 'http://localhost/loginCallback'
 firebase.initializeApp(config);
 console.log(clientid, clientsecret)
 var db = firebase.database();
-var ref = db.ref("/");
 
 router.use(Session({
   secret: '803b49765bbd574d2ee22f70b565dd661daafc5e',
@@ -80,12 +79,22 @@ router.get('/loginCallback', function (req, res, next) {
       }).then(function (data) {
         let email = data.data.emails[0].value
         if (email.slice(4, 6) == '55' && email.split('@')[1] == 'mail.nuk.edu.tw') {
-          session["stuid"] = email.split('@')[0];
+          let stuid = email.split('@')[0]
+          session["stuid"] = stuid
+          db.ref(`users/${stuid}`).once("value", snapshot => {
+            if (!snapshot.exists()) {
+              db.ref(`/users/${stuid}`).set({
+                name: email.split('@')[0],
+                grade: parseInt(email.slice(1, 4)) + 4,
+                avatar: ''
+              })
+            }
+          });
           res.render('loginCallback', {
             title: '登入成功 - 高大資工系友交流平臺',
             href: 'profile',
             status: 'success',
-            message: '登入成功，5秒後導向至首頁。'
+            message: '登入成功，3秒後導向至首頁。'
           })
         } else throw new Error('wrong email')
       }).catch(function (err) {
@@ -94,20 +103,20 @@ router.get('/loginCallback', function (req, res, next) {
           title: '登入失敗 - 高大資工系友交流平臺',
           href: 'login',
           status: 'danger',
-          message: '登入失敗，請確認是否爲高大G Suite帳號，5秒後導向至登入頁面。'
+          message: '登入失敗，請確認是否爲高大G Suite帳號，3秒後導向至登入頁面。'
         })
       })
     }
   });
 });
 
-router.get('/profile', (req,res,next)=>{
-  if (req.session['stuid']!=undefined) res.render('profile',{
+router.get('/profile', (req, res, next) => {
+  if (req.session['stuid'] != undefined) res.render('profile', {
     title: '資料 - 高大資工系友交流平臺',
-    stuid:req.session['stuid']
-  }) 
-  else{
-    res.redirect(302,'/login')
+    stuid: req.session['stuid']
+  })
+  else {
+    res.redirect(302, '/login')
     res.send()
   }
 })
@@ -115,7 +124,7 @@ router.get('/profile', (req,res,next)=>{
 router.get("/event", (req, res) => {
   res.render('event', {
     title: '高大資工系友交流平臺',
-    stuid:req.session['stuid']
+    stuid: req.session['stuid']
   })
 })
 router.get("/Twohand_Market", (req, res) => {
