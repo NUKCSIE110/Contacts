511429185a819759c57ae70ec3e586a5258fdef2
Shopping <emilyisneko@gmail.com>
2019-01-14 02:53:45 +0800

    FuckingNumber

--- public/Javascripts/market.js
+++ public/Javascripts/market.js
@@ -2,19 +2,29 @@ var database = firebase.database().ref('ProductData/Product1/1');
 var pic=document.querySelectorAll(".box a");
 var line=document.querySelectorAll(".line div");
 
+var count=0;
+var j=0;
+
 database.on('value',function(snapshot){
-     var str = '';
+     var str = [];
      var str2 = '';
        snapshot.forEach(function(data){
          for(var details in data.val()){
-           str+=data.val().picture;
-           str2+=data.val().name;
+           if(details=='name'){
+             str2+=data.val()[details]+',';
+             count++;
+           }
          }
        })
+       console.log(count)
+       for(var i=0;i<count;i++){
+         str[i]=str2.split(",")[i];
+       }
      for(var n of pic){
-         console.log(n);
      }
      for(var l of line){
-        l.textContent=str2;
+        l.textContent=str[j];
+        console.log(str[j]);
+        j++;
      }
  });
