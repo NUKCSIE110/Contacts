4df05a557cd0c2122ca1bfb2b1405e72d15b85a2
Arpan <sheep5168947@gmail.com>
2019-01-14 00:01:08 +0800

    G8

--- public/Javascripts/upload.js
+++ public/Javascripts/upload.js
@@ -9,10 +9,12 @@ btn.addEventListener('click', function () {
   var name = document.querySelector(".Pname");
   var contact = document.querySelector(".Contact");
   var price = document.querySelector(".Price");
+  var option = document.querySelector('option');
   database.push({
     "name": name.value,
     "contact": contact.value,
-    "price": price.value
+    "price": price.value,
+    "option": option.value
   })
   window.location.assign("http://localhost/twohand/market");
 })
