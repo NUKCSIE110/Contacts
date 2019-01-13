8ff502de62e809783ec953869a364266dc9f267c
ifTNT <happyjohn369@gmail.com>
2019-01-13 22:14:40 +0800

    第一版被否決掉了QQQQ 所以寫了第二版

--- public/js/zuopu.js
+++ public/js/zuopu.js
@@ -1,92 +1,59 @@
+class family{
+    constructor(_contian, _grade, _id){
+        this.contain = _contian;
+        this.grade = _grade;
+        this.id = _id;
+        this.expanded = false;
+        this.family = document.createElement('div');
+        this.family.classList.add('family');
+        this.baseNode = document.createElement('div');
+        this.baseNode.innerText = `A${this.grade}${this.id}`;
+        this.baseNode.classList.add('baseNode');
 
-var diameter = 700,
-    radius = diameter / 2,
-    innerRadius = radius - 120;
+        this.toggleExpand = this.toggleExpand.bind(this);
+        this.baseNode.addEventListener('click', this.toggleExpand);
 
-var cluster = d3.cluster()
-    .size([360, innerRadius]);
-
-var line = d3.radialLine()
-    .curve(d3.curveBundle.beta(0.85))
-    .radius(function (d) { return d.y; })
-    .angle(function (d) { return d.x / 180 * Math.PI; });
-
-var svg = d3.select(".folder").append("svg")
-    .attr("width", diameter)
-    .attr("height", diameter)
-    .append("g")
-    .attr("transform", "translate(" + radius + "," + radius + ")");
-
-var link = svg.append("g").selectAll(".link"),
-    node = svg.append("g").selectAll(".node");
-
-d3.json("flare.json", function (error, classes) {
-    if (error) throw error;
-
-    var root = packageHierarchy(classes)
-       // .sum(function (d) { return d.size; });
-
-    console.log(root);
-    cluster(root);
-
-    link = link
-        .data(packageImports(root.leaves()))
-        .enter().append("path")
-        .each(function (d) { d.source = d[0], d.target = d[d.length - 1]; })
-        .sort((a,b)=>(parseInt(a.source.data.key.substring(1),10)%100==24)?1:0)
-        .attr("class", function (d){ return (parseInt(d.source.data.key.substring(1),10)%100==24) ? "mylink" : "link"})
-        .attr("d", line);
-
-    node = node
-        .data(root.leaves())
-        .enter().append("text")
-        .attr("class", "node")
-        .attr("dy", "0.31em")
-        .attr("transform", function (d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
-        .attr("text-anchor", function (d) { return d.x < 180 ? "start" : "end"; })
-        .text(function (d) { /*return "ABC";*/return d.data.key; });
-});
-
-// Lazily construct the package hierarchy from class names.
-function packageHierarchy(classes) {
-    var map = {};
-
-    function find(name, data) {
-        var node = map[name], i;
-        if (!node) {
-            node = map[name] = data || { name: name, children: [] };
-            if (name.length) {
-                node.parent = find(name.substring(0, i = name.lastIndexOf(".")));
-                node.parent.children.push(node);
-                node.key = name.substring(i + 1);
-            }
-        }
-        return node;
+        this.upperNode = [];
+        this.family.appendChild(this.baseNode);
+        this.contain.appendChild(this.family);
+        for(let i=0; i<5; i++) this.addUpper(`A${this.grade}${this.id}`);
     }
-
-    classes.forEach(function (d) {
-        find(d.name, d);
-    });
-
-    return d3.hierarchy(map[""]);
+    addUpper(id){
+        let newUpper = document.createElement('div');
+        newUpper.classList.add('upperNode');
+        this.upperNode.push(newUpper);
+    }
+    toggleExpand(){
+        if(this.expanded){
+            this.collapse();
+        }else{
+            this.expand();
+        }
+    }
+    expand(){
+        this.expanded = true;
+        this.family.innerHTML = "";
+        for(let i of this.upperNode){
+            this.family.appendChild(i);
+        }
+        this.family.appendChild(this.baseNode);
+    }
+    collapse(){
+        this.expanded = false;
+        this.family.innerHTML = "";
+        this.family.appendChild(this.baseNode);
+    }
 }
 
-// Return a list of imports for the given array of nodes.
-function packageImports(nodes) {
-    var map = {},
-        imports = [];
-
-    // Compute a map from name to node.
-    nodes.forEach(function (d) {
-        map[d.data.name] = d;
-    });
-
-    // For each import, construct a link from the source to target node.
-    nodes.forEach(function (d) {
-        if (d.data.imports) d.data.imports.forEach(function (i) {
-            imports.push(map[d.data.name].path(map[i]));
-        });
-    });
-
-    return imports;
+class App{
+    constructor(){
+        this.familys = [];
+        for(let i=1; i<=40; i++){
+            let newFamily = new family(document.querySelector('.container'), 106, i);
+            this.familys.push(newFamily);
+        }
+        document.querySelector(".container").style.width = `${this.familys.length*220}px`;
+    }
 }
+
+var app = new App();
