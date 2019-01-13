f421ee9a5eab790f0e9b9eb4b2311f9404c0fa9b
Shopping <emilyisneko@gmail.com>
2019-01-13 16:48:56 +0800

    establish

--- public/Javascripts/establish.js
+++ public/Javascripts/establish.js
@@ -5,13 +5,12 @@ var hours=document.querySelector('.shour');
 var mins=document.querySelector('.smin');
 var ehours=document.querySelector('.ehour');
 var emins=document.querySelector('.emin');
-console.log(years)
+
 var stra='';
 var strb='';
 var strc='';
 var strd='';
 var stre='';
-
 
 stra+='<option value="all">年</option>';
 strc+='<option value="all">日</option>';
@@ -22,21 +21,9 @@ for(var i=2019;i<2101;i++){
 strb+='<option value="all">月</option>';
 for(var i=1;i<13;i++){
     strb+='<option value="'+i+'">'+i+'</option>';
-    if(i%2!=0){
-        for(var i=1;i<32;i++){
-            strc+='<option value="'+i+'">'+i+'</option>';
-        }
-    }
-    else if(i==2){
-        for(var i=1;i<29;i++){
-            strc+='<option value="'+i+'">'+i+'</option>';
-        }
-    }
-    else{
-        for(var i=1;i<31;i++){
-            strc+='<option value="'+i+'">'+i+'</option>';
-        }
-    }
+}
+for(var i=1;i<32;i++){
+    strc+='<option value="'+i+'">'+i+'</option>';
 }
 strd+='<option value="all">時</option>';
 for(var i=0;i<24;i++){
@@ -54,3 +41,9 @@ hours.innerHTML=strd;
 ehours.innerHTML=strd;
 mins.innerHTML=stre;
 emins.innerHTML=stre;
+
+var opstyle=document.querySelectorAll("option");
+
+for(var op of opstyle){
+    op.style="background-color:rgb(90, 90, 90,.75);color:white;"
+}
