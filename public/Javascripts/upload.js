9d4f11ac48bc1c8cbb8ccd41102816b42a28d678
Arpan <sheep5168947@gmail.com>
2019-01-14 00:46:29 +0800

    新增圖片

--- public/Javascripts/upload.js
+++ public/Javascripts/upload.js
@@ -18,3 +18,18 @@ btn.addEventListener('click', function () {
   })
   window.location.assign("http://localhost/twohand/market");
 })
+
+$("#uploadImage").change(function () {
+  readImage(this);
+});
+
+function readImage(input) {
+  if (input.files && input.files[0]) {
+      var FR = new FileReader();
+      FR.onload = function (e) {
+          //e.target.result = base64 format picture
+          $('#img').attr("src", e.target.result);
+      };
+      FR.readAsDataURL(input.files[0]);
+  }
+}
